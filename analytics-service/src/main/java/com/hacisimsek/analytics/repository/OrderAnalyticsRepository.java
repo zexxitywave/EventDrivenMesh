b47366bd67a7f9cae24d0e768d156e4639c5bc23
zexxitywave <zexxitywave@users.noreply.github.com>
2026-08-29 19:56:52 +0530

    fix(analytics): update OrderAnalyticsRepository

--- analytics-service/src/main/java/com/hacisimsek/analytics/repository/OrderAnalyticsRepository.java
+++ analytics-service/src/main/java/com/hacisimsek/analytics/repository/OrderAnalyticsRepository.java
@@ -4,7 +4,6 @@ import com.hacisimsek.analytics.model.OrderAnalytics;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.math.BigDecimal;
 import java.util.List;
 
