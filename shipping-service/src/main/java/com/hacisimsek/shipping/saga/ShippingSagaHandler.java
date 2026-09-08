8727e801bc45c09b1aa049b9242e6a07b6d9e9e5
zexxitywave <zexxitywave@users.noreply.github.com>
2026-09-09 05:14:37 +0530

    fix: shipping saga compare by class name and convertValue

--- shipping-service/src/main/java/com/hacisimsek/shipping/saga/ShippingSagaHandler.java
+++ shipping-service/src/main/java/com/hacisimsek/shipping/saga/ShippingSagaHandler.java
@@ -1,4 +1,6 @@
 package com.hacisimsek.shipping.saga;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import com.hacisimsek.common.event.payment.PaymentProcessedEvent;
 import com.hacisimsek.shipping.service.ShippingService;
@@ -12,7 +14,11 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class ShippingSagaHandler {
 
+    private static final String PAYMENT_PROCESSED = PaymentProcessedEvent.class.getName();
+
     private final ShippingService shippingService;
+    private final ObjectMapper objectMapper;
+
     // Shipping is triggered by a successful payment.
 
     @KafkaListener(
@@ -23,15 +29,17 @@ public class ShippingSagaHandler {
     public void handlePaymentEvents(ConsumerRecord<String, Object> record) {
 
         Object event = record.value();
+        String type = event != null ? event.getClass().getName() : null;
+        log.info("[ShippingSagaHandler] payment-events type={}", type);
 
-        log.info("Received event class: {}", event.getClass().getName());
-
-        if (event instanceof PaymentProcessedEvent paymentProcessedEvent) {
-
+        if (PAYMENT_PROCESSED.equals(type)) {
+            PaymentProcessedEvent paymentProcessedEvent =
+                    objectMapper.convertValue(event, PaymentProcessedEvent.class);
             log.info("Received PaymentProcessedEvent for order: {}",
                     paymentProcessedEvent.getOrderId());
-
             shippingService.processShipping(paymentProcessedEvent);
+        } else {
+            log.debug("[ShippingSagaHandler] Ignoring non-PaymentProcessedEvent on payment-events: {}", type);
         }
     }
 }
